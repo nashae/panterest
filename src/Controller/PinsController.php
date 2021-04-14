79f153b353ff7fa6b66262eb41d37016cb48e4dc
nashae <kesmoya@gmail.com>
2021-04-14 16:36:03 +0200

    delete pin

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -69,5 +69,13 @@ class PinsController extends AbstractController
             'pin' => $pin
         ]);
     }
+
+    #[Route("/pins/{id<[0-9]+>}/delete", name:"app_pins_delete", methods:["DELETE"])]
+    public function delete(Pin $pin):Response
+    {
+        $this->em->remove($pin);
+        $this->em->flush();
+        return $this->redirectToRoute("app_home");
+    }
 }
 
