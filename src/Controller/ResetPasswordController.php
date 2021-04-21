ebe220260f81ace0dc46bf04980131aed205e3e0
nashae <kesmoya@gmail.com>
2021-04-21 17:55:10 +0200

    limit access to reset password to connected users

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -36,6 +36,11 @@ class ResetPasswordController extends AbstractController
     #[Route('', name: 'app_forgot_password_request')]
     public function request(Request $request, MailerInterface $mailer): Response
     {
+        if($this->getUser()){
+            $this->addFlash('error', 'Already logged in');
+            return $this->redirectToRoute('app_home');
+        } 
+        
         $form = $this->createForm(ResetPasswordRequestFormType::class);
         $form->handleRequest($request);
 
