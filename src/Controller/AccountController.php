1b40d617a9c7fda05ce942caf9fdf72a90ad92e1
nashae <kesmoya@gmail.com>
2021-04-22 10:47:42 +0200

    add change password feature

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,15 +2,53 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordFormType;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
-    #[Route('/account', name: 'app_account')]
+    #[Route('/account', name: 'app_account', methods: ['GET'])]
     public function show(): Response
     {
         return $this->render('account/show.html.twig');
+    }
+
+    #[Route('/account/edit', name: 'app_account_edit', methods: ["GET", "POST"])]
+    public function edit(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+            $this->addFlash('success', "Account updated successfully");
+            return $this->redirectToRoute('app_account');
+        }
+        return $this->render('account/edit.html.twig', [
+            'form' => $form->createview()
+        ]);
+    }
+
+    #[Route("account/change-password", name: "app_account_change_password", methods: ["GET", "POST"])]
+    public function changePassword(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder, ): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordFormType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $user->setPassword($passwordEncoder->encodePassword($user, $form->get('plainPassword')->getData()));
+            $em->flush();
+            $this->addFlash('success', "Password updated");
+            return $this->redirectToRoute('app_account');
+        }
+        return $this->render('account/change_password.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
